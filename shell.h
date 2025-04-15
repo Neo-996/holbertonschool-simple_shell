7b5209f58064f69c6ed34b6dc0ac78a4b3c23ceb
root <abdulelahmalshehri@gmail.com>
2025-04-15 16:29:28 +0000

    adding header file

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/* Global environment pointer */
+extern char **environ;
+
+/* Function Prototypes */
+void execute_cmd(char *line, char *prog_name, int line_num);
+char **tokenize_input(char *line);
+void free_args(char **args);
+void print_error(char *prog_name, int line_num, char *command);
+
+#endif /* SHELL_H */
