7e883d9845b19056141b147e27aa4c206c95201e
root <abdulelahmaslhehri@gmail.com>
2025-04-16 16:18:53 +0000

    defining free_agrs()

--- utils.c
+++ utils.c
@@ -10,3 +10,22 @@ void print_error(char *prog_name, int line_num, char *command)
 {
   fprintf(stderr, "%s: %d: %s: not found\n", prog_name, line_num, command);
 }
+
+/**
+ * free_args - Frees a NULL-terminated array of strings
+ * @args: The array to free
+ */
+void free_args(char **args)
+{
+   int i = 0;
+
+   if (!args)
+     return;
+
+   while (args[i])
+     {
+       free(args[i]);
+       i++;
+     }
+   free(args);
+}
