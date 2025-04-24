7d595e569caf4b892b6bed5699af4ba26958088b
root <Abrarsaadalharbi@gmail.com>
2025-04-24 06:39:00 +0000

    code edit

--- execution.c
+++ execution.c
@@ -1,97 +1,125 @@
 #include "shell.h"
 
+/**
+ * handle_builtins - Identifies and runs built-in commands
+ * @args: Array of command arguments
+ * Return: 1 if a built-in command was executed, 0 if not
+ */
+int handle_builtins(char **args)
+{
+	int i;
 
-int run_command(char *cmd_path, char **args);
+	/* Process the built-in "exit" command */
+	if (strcmp(args[0], "exit") == 0)
+		exit(0);
+	/* Process the built-in "env" command */
+	if (strcmp(args[0], "env") == 0)
+	{
+		for (i = 0; environ[i]; i++)
+			printf("%s\n", environ[i]);
+		return (1); /* Indicates a built-in command was executed */
+	}
+
+	return (0); /* Not a built-in command */
+}
+
+/**
+  * resolve_command_path - Finds the full path of a command
+ * @args: Array of command arguments
+ * Return: Full path to the command if found, otherwise NULL
+ */
+char *resolve_command_path(char **args)
+{
+	char *cmd_path = NULL;
+
+	/* If the command contains '/', assume it's a path */
+	if (strchr(args[0], '/'))
+	{
+		if (access(args[0], X_OK) == 0)
+			cmd_path = strdup(args[0]); /* Use the given path */
+		else
+			return (NULL);
+	}
+	else
+	{
+		/* Free memory used for command path */
+		cmd_path = find_command(args[0]);
+	}
+
+	return (cmd_path);
+}
+
+/**
+ * run_command - Forks and executes a command
+ * @cmd_path: Full path to command
+ * @args: Argument vector
+ * Return: Command exit status or 0
+ */
+int run_command(char *cmd_path, char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+
+	if (pid == 0)
+	{
+		/* Child process: Execute the command */
+		execve(cmd_path, args, environ);
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		free(cmd_path);
+		exit(127);
+	}
+	else if (pid < 0)
+	{
+		/* Fork failed */
+		perror("fork");
+	}
+	else
+	{
+		/* Parent process: Wait for the child to finish */
+		if (waitpid(pid, &status, 0) != -1)
+		{
+			if (WIFEXITED(status))
+				return (WEXITSTATUS(status));
+		}
+	}
+
+	return (0);
+}
 
 /**
  * execute_cmd - Executes a command with path resolution
  * @args: Argument vector (command and its arguments)
- * Return: 127 if command not found, 0 otherwise
+ * Return: 127 if command not found, or exit status
  */
 int execute_cmd(char **args)
 {
-int status;
-char *cmd_path;
+	char *cmd_path;
+	int exit_status = 0;
 
-if (args[0] == NULL)
-return (0);
-/* Built-in: env */
-if (strcmp(args[0], "env") == 0)
-{
-int i = 0;
-while (environ[i])
-printf("%s\n", environ[i++]);
-return (0);
-}
-/* Check if command contains '/' */
-if (strchr(args[0], '/'))
-{
-if (access(args[0], X_OK) == 0)
-cmd_path = strdup(args[0]);
-else
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-return (127);
-}
-}
-else
-{
-/* No '/', resolve using PATH */
-cmd_path = find_command(args[0]);
-if (cmd_path == NULL)
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-return (127);
-}
-}
-/* Fork and exec */
-status = run_command(cmd_path, args);
-free(cmd_path);
-return (status);
-}
+	if (args[0] == NULL)
+		return (0);
 
-/**
- * run_command - Forks and executes the command
- * @cmd_path: Resolved command path
- * @args: Argument vector
- * Return: Exit status
- */
-int run_command(char *cmd_path, char **args)
-{
+	/* Check and handle built-in commands */
+	if (handle_builtins(args))
+		return (0);
 
-pid_t pid;
-int status;
+	/* Resolve the full path of the command */
+	cmd_path = resolve_command_path(args);
+	if (cmd_path == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		return (127);
+	}
 
-pid = fork();
+	exit_status = run_command(cmd_path, args);
 
-if (pid == 0)
-{
-if (execve(cmd_path, args, environ) == -1)
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-if (cmd_path != NULL)
-{
-free(cmd_path);
-}
-exit(127);
-}
-}
-else if (pid < 0)
-{
-perror("fork");
-return (1);
-}
-else
-{
-waitpid(pid, &status, 0);
-if (WIFEXITED(status))
-status = WEXITSTATUS(status);
-else
-status = 1;
-}
-if (cmd_path != NULL)
-{
-free(cmd_path);
-}
-return (1);
+	if (cmd_path != NULL)
+	{
+		free(cmd_path);
+		cmd_path = NULL;
+	}
+
+	return (exit_status);
 }
